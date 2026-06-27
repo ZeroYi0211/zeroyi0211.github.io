54329df4551549f5e8428317929fedcb01c2aed3
YiKejun <ykj42960@163.com>
2026-06-27 11:32:14 +0800

    Highlight wrong practice choices

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1782530335',resources: [
+    cacheName: 'chirpy-1782531133',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
