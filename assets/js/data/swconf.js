e08fbee5405280ca7d7823257f091993d3b6d663
YiKejun <ykj42960@163.com>
2026-06-28 22:14:40 +0800

    修正机械制造刷题解析

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1782655622',resources: [
+    cacheName: 'chirpy-1782656079',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
