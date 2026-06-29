d3ecd78431f9e17f083b2b6b5c4e7a7418dde9e3
YiKejun <ykj42960@163.com>
2026-06-29 18:08:43 +0800

    更新成型刷题舱翻译并刷新缓存版本

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1782727570',resources: [
+    cacheName: 'chirpy-1782727723',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
