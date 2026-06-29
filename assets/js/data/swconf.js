6f8fc19ab4f3a2136dcf968057ef7fbe597d4cb5
YiKejun <ykj42960@163.com>
2026-06-29 18:17:19 +0800

    修复成型刷题舱错误选项高亮

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1782727723',resources: [
+    cacheName: 'chirpy-1782728238',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
